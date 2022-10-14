d15a09ab94a05ce4aadf4e4838f713eb0603f1e5
Rabeya Bosri <67573017+rabeya15dev@users.noreply.github.com>
2022-10-14 22:26:38 +0600

    Add files via upload
    my 2nd

---
+++ main (2).py
@@ -0,0 +1,7 @@
+﻿plant = input("Enter your plant:")
+if plant == "Spathiphyllum" :
+    print("Yes - Spathiphyllum is the best plant ever!")
+if plant == "spathiphyllum" :
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not [input]!",plant)
