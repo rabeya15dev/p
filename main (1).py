bcc6afe55e60598180e9ee07236f926497386603
Rabeya Bosri <67573017+rabeya15dev@users.noreply.github.com>
2022-10-14 20:59:00 +0600

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,37 @@
+﻿#number1 = int(input("Enter the first NUmber:"))
+#number2 = int(input("Enter the second NUmber:"))
+ 
+#if number1 > number2:
+     #larger_number = number1
+#else:
+    #     larger_number = number2
+         
+#print("The larger Number is:",larger_number)
+
+
+#number1 = int(input("Enter The first Number:"))
+#number2 = int(input("ENter yhe second Number:"))
+
+#if number1 > number2: larger_number =number1
+#else:larger_number =number2
+
+#print("The Larger NUmber is:",larger_number)
+
+
+
+number1 = int(input("Enter the first NUmber:"))
+number2 = int(input("Enter the second NUmber:"))
+number3 = int(input("Enter the third NUmber:"))
+
+largest_number = number1
+
+if number2 > largest_number:
+    largest_number =number2
+    
+if number3 >largest_number:
+    largest_number =number3
+
+print("The largest NUmber is:",largest_number)
+
+
+
